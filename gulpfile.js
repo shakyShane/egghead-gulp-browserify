9b422a193629ac28917d29bc2fbd19c7a9e59974
Shane Osbourne <shane.osbourne8@gmail.com>
2015-09-11 20:07:28 +0100

    Code for lesson 'Adding Live Reload with Browsersync'

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gutil = require('gulp-util');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var watchify = require('watchify');
+var browserSync = require('browser-sync').create();
 
 function bundle (bundler) {
     return bundler
@@ -11,19 +12,22 @@ function bundle (bundler) {
             gutil.log(e.message);
         })
         .pipe(source('bundle.js'))
-        .pipe(gulp.dest('./app/js/dist'));
+        .pipe(gulp.dest('./app/js/dist'))
+        .pipe(browserSync.stream());
 }
 
 gulp.task('watch', function () {
     var watcher = watchify(browserify('./app/js/app.js', watchify.args));
-
     bundle(watcher);
-
     watcher.on('update', function () {
         bundle(watcher);
     });
-
     watcher.on('log', gutil.log);
+
+    browserSync.init({
+        server: './app',
+        logFileChanges: false
+    });
 });
 
 gulp.task('js', function () {
