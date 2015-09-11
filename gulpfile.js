569a98e2e398bed20e7decbe6d3990b88cf1acf6
Shane Osbourne <shane.osbourne8@gmail.com>
2015-09-11 20:03:59 +0100

    Code for lesson 'Hooking up Watchify'

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,30 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
+var watchify = require('watchify');
 
-gulp.task('js', function () {
-    return browserify('./app/js/app.js')
+function bundle (bundler) {
+    return bundler
         .bundle()
         .on('error', function (e) {
             gutil.log(e.message);
         })
         .pipe(source('bundle.js'))
         .pipe(gulp.dest('./app/js/dist'));
+}
+
+gulp.task('watch', function () {
+    var watcher = watchify(browserify('./app/js/app.js', watchify.args));
+
+    bundle(watcher);
+
+    watcher.on('update', function () {
+        bundle(watcher);
+    });
+
+    watcher.on('log', gutil.log);
+});
+
+gulp.task('js', function () {
+    return bundle(browserify('./app/js/app.js'));
 });
